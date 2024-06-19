412870d989ac8750cf88e547a70db4ce42a22823
inbaseman <inbaseman@gmail.com>
2024-06-19 20:04:01 +0900

    landingPage_workingOn

--- js/clon_studiolico_index.js
+++ js/clon_studiolico_index.js
@@ -14,100 +14,114 @@ const sec03_sub = document.querySelector('.sec03_sub');
 const sec03_link = document.querySelector('.sec03_link');
 
 
-sec01_main.classList.add("animate__animated");
-sec01_main.classList.add("animate__fadeInUp");
-
-sec01_sub.classList.add("animate__animated");
-sec01_sub.classList.add("animate__fadeInUp");
-
-sec01_link.classList.add("animate__animated");
-sec01_link.classList.add("animate__fadeInUp");
-
-
-swiper_button_next.addEventListener('click', function () {
-  if (!sec01.classList.contains("blind")) {
-
-    sec01.classList.add("animate__animated");
-    sec01.classList.add("animate__fadeOut");
-
-    setTimeout(() => {
-      sec01.classList.add("blind");
-      sec01_main.classList.add("blind");
-      sec01_sub.classList.add("blind");
-      sec01_link.classList.add("blind");
-    }, 1000);
+const no1 = document.querySelector('.no1');
+const no2 = document.querySelector('.no2');
+const no3 = document.querySelector('.no3');
+const no4 = document.querySelector('.no4');
+const no5 = document.querySelector('.no5');
+const no6 = document.querySelector('.no6');
+const no7 = document.querySelector('.no7');
+const no8 = document.querySelector('.no8');
+const no9 = document.querySelector('.no9');
+const swiper_slide = document.querySelector('.sec');
 
 
-    setTimeout(() => {
-      sec02.classList.remove("blind");
-    }, 800);
-    setTimeout(() => {
-      sec02_main.classList.remove("blind");
-      sec02_main.classList.add("animate__animated");
-      sec02_main.classList.add("animate__fadeInUp");
-      sec02_sub.classList.remove("blind");
-      sec02_sub.classList.add("animate__animated");
-      sec02_sub.classList.add("animate__fadeInUp");
-      sec02_link.classList.remove("blind");
-      sec02_link.classList.add("animate__animated");
-      sec02_link.classList.add("animate__fadeInUp");
-    }, 800);
-
-  } else if (!sec02.classList.contains("blind")) {
-
-    sec02.classList.add("animate__animated");
-    sec02.classList.add("animate__fadeOut");
-    setTimeout(() => {
-      sec02.classList.add("blind");
-      sec02_main.classList.add("blind");
-      sec02_sub.classList.add("blind");
-      sec02_link.classList.add("blind");
-    }, 1000);
 
 
-    setTimeout(() => {
-      sec03.classList.remove("blind");
-    }, 800);
-    setTimeout(() => {
-      sec03_main.classList.remove("blind");
-      sec03_main.classList.add("animate__animated");
-      sec03_main.classList.add("animate__fadeInUp");
-      sec03_sub.classList.remove("blind");
-      sec03_sub.classList.add("animate__animated");
-      sec03_sub.classList.add("animate__fadeInUp");
-      sec03_link.classList.remove("blind");
-      sec03_link.classList.add("animate__animated");
-      sec03_link.classList.add("animate__fadeInUp");
-    }, 800);
+// sec01_main.classList.add("animate__animated");
+// sec01_main.classList.add("animate__fadeInUp");
 
-  } else if (!sec03.classList.contains("blind")) {
+// sec01_sub.classList.add("animate__animated");
+// sec01_sub.classList.add("animate__fadeInUp");
 
-    sec03.classList.add("animate__animated");
-    sec03.classList.add("animate__fadeOut");
-    setTimeout(() => {
-      sec03.classList.add("blind");
-      sec03_main.classList.add("blind");
-      sec03_sub.classList.add("blind");
-      sec03_link.classList.add("blind");
-    }, 1000);
+// sec01_link.classList.add("animate__animated");
+// sec01_link.classList.add("animate__fadeInUp");
 
 
-    setTimeout(() => {
-      sec01.classList.remove("blind");
-    }, 800);
-    setTimeout(() => {
-      sec01_main.classList.remove("blind");
-      sec01_main.classList.add("animate__animated");
-      sec01_main.classList.add("animate__fadeInUp");
-      sec01_sub.classList.remove("blind");
-      sec01_sub.classList.add("animate__animated");
-      sec01_sub.classList.add("animate__fadeInUp");
-      sec01_link.classList.remove("blind");
-      sec01_link.classList.add("animate__animated");
-      sec01_link.classList.add("animate__fadeInUp");
-    }, 800);
-  }
-});
+// swiper_button_next.addEventListener('click', function () {
+//   if (!sec01.classList.contains("blind")) {
+
+//     sec01.classList.add("animate__animated");
+//     sec01.classList.add("animate__fadeOut");
+
+//     setTimeout(() => {
+//       sec01.classList.add("blind");
+//       sec01_main.classList.add("blind");
+//       sec01_sub.classList.add("blind");
+//       sec01_link.classList.add("blind");
+//     }, 1000);
+
+
+//     setTimeout(() => {
+//       sec02.classList.remove("blind");
+//     }, 800);
+//     setTimeout(() => {
+//       sec02_main.classList.remove("blind");
+//       sec02_main.classList.add("animate__animated");
+//       sec02_main.classList.add("animate__fadeInUp");
+//       sec02_sub.classList.remove("blind");
+//       sec02_sub.classList.add("animate__animated");
+//       sec02_sub.classList.add("animate__fadeInUp");
+//       sec02_link.classList.remove("blind");
+//       sec02_link.classList.add("animate__animated");
+//       sec02_link.classList.add("animate__fadeInUp");
+//     }, 800);
+
+//   } else if (!sec02.classList.contains("blind")) {
+
+//     sec02.classList.add("animate__animated");
+//     sec02.classList.add("animate__fadeOut");
+//     setTimeout(() => {
+//       sec02.classList.add("blind");
+//       sec02_main.classList.add("blind");
+//       sec02_sub.classList.add("blind");
+//       sec02_link.classList.add("blind");
+//     }, 1000);
+
+
+//     setTimeout(() => {
+//       sec03.classList.remove("blind");
+//     }, 800);
+//     setTimeout(() => {
+//       sec03_main.classList.remove("blind");
+//       sec03_main.classList.add("animate__animated");
+//       sec03_main.classList.add("animate__fadeInUp");
+//       sec03_sub.classList.remove("blind");
+//       sec03_sub.classList.add("animate__animated");
+//       sec03_sub.classList.add("animate__fadeInUp");
+//       sec03_link.classList.remove("blind");
+//       sec03_link.classList.add("animate__animated");
+//       sec03_link.classList.add("animate__fadeInUp");
+//     }, 800);
+
+//   } else if (!sec03.classList.contains("blind")) {
+
+//     sec03.classList.add("animate__animated");
+//     sec03.classList.add("animate__fadeOut");
+//     setTimeout(() => {
+//       sec03.classList.add("blind");
+//       sec03_main.classList.add("blind");
+//       sec03_sub.classList.add("blind");
+//       sec03_link.classList.add("blind");
+//     }, 1000);
+
+
+//     setTimeout(() => {
+//       sec01.classList.remove("blind");
+//     }, 800);
+//     setTimeout(() => {
+//       sec01_main.classList.remove("blind");
+//       sec01_main.classList.add("animate__animated");
+//       sec01_main.classList.add("animate__fadeInUp");
+//       sec01_sub.classList.remove("blind");
+//       sec01_sub.classList.add("animate__animated");
+//       sec01_sub.classList.add("animate__fadeInUp");
+//       sec01_link.classList.remove("blind");
+//       sec01_link.classList.add("animate__animated");
+//       sec01_link.classList.add("animate__fadeInUp");
+//     }, 800);
+//   }
+// });
 
 
 
